5d2ac5992f1f97f6b2742b45a6718a57bb3556a7
Vince Nguyen <81478088+vinny-nguyen@users.noreply.github.com>
2024-02-13 10:25:02 -0400

    ScannerInput
    Feb 13, 2024:

--- ScannerInput.java
+++ ScannerInput.java
@@ -1,35 +1,32 @@
-import java.util.Scanner; //imports the package from java API
-
+import java.util.Scanner; // imports the package from java API
 public class ScannerInput {
     public static void main(String[] args) throws Exception {
-        //Declare variables used in the program
+        // Declare variables used in the program
         int num1;
         double num2;
         double sum;
         double product;
         double div;
         String name;
-        
-        //Initialize a scanner object
+
+        // Initialize a scanner object
         Scanner scan = new Scanner(System.in);
 
-        //Read Input From keyboard
-        System.out.print("Hello User, Please Enter An Integer: "); //this will show a prompt in your terminal
-        num1 = scan.nextInt(); //This reads the value from the prompt after pressing 'enter'
-        System.out.print("Enter a float: "); //this will show a prompt in your terminal
-        num2 = scan.nextDouble(); //This reads the value from the prompt after pressing 'enter'
-        System.out.print("Enter your name: ");
-        name = scan.next(); 
+        //Read input from keyboard
+        System.out.println("Enter your name: ");
+        name = scan.next();
+        System.out.println("Hello " + name +", please enter an integer: ");
+        num1 = scan.nextInt(); // reads the value from user input
+        System.out.println("Enter a float: ");
+        num2 = scan.nextDouble();
 
-        //Working with the stored input
-        sum = num1 + num2;
-        product = num1 * num2;
-        div = num1 / num2;
-
-        System.out.println("Thank you " + name);
-        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
-        System.out.println("Product of " + num1 + " and " + num2 + " is: " + product);
-        System.out.println("Division of " + num1 + " and " + num2 + " is: " + div);
+        sum = num1+num2;
+        product = num1*num2;
+        div = num1/num2;
+        System.out.println("Thank you, " + name +".");
+        System.out.println("Sum of: " + num1 + " and " + num2 + " is: " + sum + ".");
+        System.out.println("Product of: " + num1 + " and " + num2 + " is: " + product + ".");
+        System.out.println("Division of: " + num1 + " and " + num2 + " is: " + div + ".");
         scan.close();
     }
 }
