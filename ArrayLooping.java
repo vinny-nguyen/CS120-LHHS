5dca80fa207befd0d47d73b82927814bef4b3b73
Vince Nguyen <81478088+vinny-nguyen@users.noreply.github.com>
2024-03-23 12:07:47 -0300

    Looping through Arrays March 23, 2024 complete

---
+++ ArrayLooping.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+public class ArrayLooping {
+    public static void main(String[] args) throws Exception {
+        //boolean solve = true;
+        Scanner scan = new Scanner(System.in);
+        boolean [] status = {true, true, false, true};
+        for (int i = 0; i < status.length; i++){
+            while (!status[i]){
+                JOptionPane.showMessageDialog(null, "The alarm has been triggered.");
+                status[3] = true;
+                String statusInputString = JOptionPane.showInputDialog("Has The Problem Been Resolved? (Y/N): ");
+                if (statusInputString.equalsIgnoreCase("Y")) {
+                    status[i] = true;
+                    scan.close();
+                } else {
+                    status[3] = false;
+                }
+            }            
+        }
+
+
+    }
+}
