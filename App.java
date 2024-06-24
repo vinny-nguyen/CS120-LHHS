0b18ce9ded6cb15ead10ad234e1375f34eaa1a0d
Vince Nguyen <81478088+vinny-nguyen@users.noreply.github.com>
2024-06-24 15:16:34 -0300

    Computer Science 120 with Mr. McDowell (2024)

---
+++ App.java
@@ -0,0 +1,18 @@
+public class App {
+
+    public void App1(){
+      //comment
+      /* comment 
+       * in
+       * bullet points
+      */
+    }
+    public static void main(String[] args) throws Exception {
+      System.out.println("Hello World");
+      int num1 = 1;
+      int num2, num3 = 2;
+      int product;
+      product = num1*num2 + num3;
+      System.out.printIn(product);
+    }
+  }
---
+++ Randomapp.java
@@ -0,0 +1,32 @@
+public class Randomapp {
+
+    @Override
+    public String toString() {
+        return "Randomapp []";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+    
+}
